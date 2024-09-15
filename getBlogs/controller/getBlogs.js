97de1fd05d5c82d78010ac96f4da3263b67b5666
SarthakMoriya <sarthak8544@gmail.com>
2024-09-15 19:59:51 +0530

    added get+redis+nats+locally

--- getBlogs/controller/getBlogs.js
+++ getBlogs/controller/getBlogs.js
@@ -1,28 +1,81 @@
+import { connectRedis, initializeNATS } from "../index.js";
 import Blog from "../models/blogModel.js";
-import nats from 'nats';
+import nats from "nats";
+
+export const handleNats = async () => {
+  try {
+    const redis = await connectRedis();
+    const nc = await initializeNATS();
+    const sc = nats.StringCodec();
+    if (redis == null) throw new Error("Error connecting to REDIS");
+    if (nc == null) throw new Error("Error connecting to NATS");
+    await nc.subscribe("delete", (msg1) => {
+      const data1 = sc.decode(msg1.data);
+      console.log("Received message from topic D:", data1);
+      // Handle topic D message
+    });
+
+    await nc.subscribe("create", (msg2) => {
+      const data2 = sc.decode(msg2.data);
+      console.log("Received message from topic C:", data2);
+      // Handle topic C message
+    });
+    // SUBSCRIBE TO ALL TOPICS
+    const topic_d = nc.subscribe("delete");
+    const topic_c = nc.subscribe("create");
+    const topic_u = nc.subscribe("update");
+
+    //HANDLE EACH TOPIC
+    const handleTopicD = async () => {
+      try {
+        for await (const msg1 of topic_d) {
+          console.log("Received message from delete:", sc.decode(msg1.data));
+          const data = JSON.parse(sc.decode(msg1.data));
+
+          await redis.hDel("blogs", data.data);
+        }
+      } catch (error) {
+        console.log("Error deleting cache");
+      }
+    };
+    const handleTopicC = async () => {
+      try {
+        for await (const msg of topic_c) {
+          const data = JSON.parse(sc.decode(msg.data));
+          let { _id, ...others } = data.data;
+          await redis.hSet("blogs", _id, JSON.stringify({ ...others }));
+        }
+      } catch (error) {
+        console.log("Error setting cache...");
+      }
+    };
+    const handleTopicU = async () => {
+      try {
+        for await (const msg of topic_u) {
+          const { data } = JSON.parse(sc.decode(msg.data));
+          const { _id, ...others } = data;
+          await redis.hSet("blogs", _id, JSON.stringify({ ...others }));
+        }
+      } catch (error) {
+        console.log("Error setting cache...");
+      }
+    };
+
+    // Run all handlers concurrently
+    await Promise.all([handleTopicD(), handleTopicC(), handleTopicU()]);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getBlogs = async (req, res) => {
   try {
     const blogs = await Blog.find();
+    handleNats();
 
-    const nc = await nats.connect({ servers: ["nats://nats:4222"] });
-    console.log(`connected to NATS server :--${nc.getServer()}`);
-    const sc = nats.StringCodec();
-    nc.closed().then(() => {
-      console.log('Connection closed');
-    }).catch((err) => {
-      console.error('NATS connection error:', err);
-    });
-
-    const sub = nc.subscribe("create");
-    (async () => {
-      for await (const msg of sub) {
-        console.log('Received message:', sc.decode(msg.data));
-      }
-    })();
     res.send(blogs);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.send(error);
   }
 };
--- getBlogs/index.js
+++ getBlogs/index.js
@@ -2,18 +2,47 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes/routes.js";
+import nats from "nats";
+import { createClient } from "redis";
+import { handleNats } from "./controller/getBlogs.js";
 
+let pool;
+export const initializeNATS = async () => {
+  try {
+    if (!pool) pool = await nats.connect({servers:["nats://localhost:4222"]});
+    console.log(`NATS Connected at ${pool.getServer()}`);
+    return pool;
+  } catch (error) {
+    console.log("Error connecting to NATS!");
+    pool=null;
+  }
+};
+
+export const connectRedis = async () => {
+  let client = await createClient()
+    .on("error", (err) => {
+      console.log("Redis Client Error", err);
+    })
+    .connect();
+  console.log("Redis Connected!");
+  return client;
+};
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  `mongodb+srv://sarthak:AP945cfylvOTjkTz@cluster0.3ner1bf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-);
+mongoose
+  .connect(
+    `mongodb+srv://sarthak:p9wEwyQQbCAiNZPA@cluster0.dkryym7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .catch((err) => console.log("Error connecting to DB"));
 
-app.use('/get',router)
+app.use("/get", router);
 
 app.listen(3003, () => {
+  // initializeNATS();
+  // connectRedis();
+  handleNats()
   console.log("Get server listening on port 3003");
 });
